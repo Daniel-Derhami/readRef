045a19887eb4019f807f364d48254cf8208edb39
DanDer <Dartanian@Dartanian-PC>
2017-02-11 21:37:32 +0100

    Input/Scanner

--- readRef/src/main/Main.java
+++ readRef/src/main/Main.java
@@ -5,14 +5,14 @@
  */
 package main;
 
+import java.util.Scanner;
+
 public class Main {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		System.out.println("Enter a String: ");
+		Scanner scan = new Scanner(System.in);
 	}
 
 }
